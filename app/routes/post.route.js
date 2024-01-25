9ec0e7932a66cecf4c21bccf6b49c9534bb98a25
Habeeb Dindi <habeebullahdindi@gmail.com>
2024-01-25 02:37:06 +0100

    Add new route to get author's posts, remove former one

--- app/routes/post.route.js
+++ app/routes/post.route.js
@@ -5,8 +5,8 @@ const controller = require("../controllers/post.controller");
 
 
 router.get('/posts', [authJwt.verifyToken], controller.allPosts);
+router.get('/post/author/:author', [authJwt.verifyToken], controller.authorPosts);
 router.get('/post/:postId', [authJwt.verifyToken], controller.onePost);
-router.get('/post/:author', [authJwt.verifyToken], controller.authorPosts);
 router.post('/post', [authJwt.verifyToken], controller.createPost);
 router.put('/post/:postId', [authJwt.verifyToken], controller.updatePost);
 router.delete('/post/:postId', [authJwt.verifyToken], controller.deletePost);
